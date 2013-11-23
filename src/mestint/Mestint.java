a69fbc340b15e270df9116ec93ea36e8be6f22e9
prometh <andriko.gergely@hotmail.com>
2013-11-23 10:02:38 +0100

    alap

---
+++ src/mestint/Mestint.java
@@ -0,0 +1,15 @@
+/*
+Mesterséges Intelligencia feladat
+@author Andrikó Gergely, Veress Tibor
+ */
+
+package mestint;
+
+public class Mestint {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
